51a058259aae34f4eb904733e5ab08b6c239cebf
杨俊艺 <1209032479@qq.com>
2018-05-31 09:34:03 +0800

    FirstVersion

--- CSharpSocket/Form1.Designer.cs
+++ CSharpSocket/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Btn_Start = new System.Windows.Forms.Button();
+            this.RichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.RichTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.Btn_Start);
+            this.GroupBox1.Controls.Add(this.Label1);
+            this.GroupBox1.Location = new System.Drawing.Point(12, 12);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(750, 100);
+            this.GroupBox1.TabIndex = 0;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "服务器状态";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(20, 50);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(152, 18);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "无客户机请求连接";
+            // 
+            // Btn_Start
+            // 
+            this.Btn_Start.Location = new System.Drawing.Point(623, 48);
+            this.Btn_Start.Name = "Btn_Start";
+            this.Btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Start.TabIndex = 1;
+            this.Btn_Start.Text = "启动";
+            this.Btn_Start.UseVisualStyleBackColor = true;
+            this.Btn_Start.Click += new System.EventHandler(this.Btn_Start_Click);
+            // 
+            // RichTextBox1
+            // 
+            this.RichTextBox1.Location = new System.Drawing.Point(12, 150);
+            this.RichTextBox1.Name = "RichTextBox1";
+            this.RichTextBox1.Size = new System.Drawing.Size(750, 150);
+            this.RichTextBox1.TabIndex = 1;
+            this.RichTextBox1.Text = "";
+            // 
+            // RichTextBox2
+            // 
+            this.RichTextBox2.Location = new System.Drawing.Point(12, 350);
+            this.RichTextBox2.Name = "RichTextBox2";
+            this.RichTextBox2.Size = new System.Drawing.Size(750, 150);
+            this.RichTextBox2.TabIndex = 2;
+            this.RichTextBox2.Text = "";
+            this.RichTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RichTextBox2_KeyPress);
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(9, 129);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(89, 18);
+            this.Label2.TabIndex = 3;
+            this.Label2.Text = "聊天记录:";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(9, 329);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(62, 18);
+            this.Label3.TabIndex = 4;
+            this.Label3.Text = "输入框";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.RichTextBox2);
+            this.Controls.Add(this.RichTextBox1);
+            this.Controls.Add(this.GroupBox1);
+            this.Name = "Form1";
+            this.Text = "服务器端";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox GroupBox1;
+        private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.RichTextBox RichTextBox1;
+        private System.Windows.Forms.RichTextBox RichTextBox2;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label3;
     }
 }
 
--- CSharpSocket/Form1.cs
+++ CSharpSocket/Form1.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
 
 namespace CSharpSocket
 {
@@ -15,6 +19,115 @@ namespace CSharpSocket
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;//禁用此异常
+        }
+
+        private bool bConnected = false;//客户机与服务器之间的连接状态
+        private Thread tAcceptMsg = null;//侦听线程
+        private IPEndPoint IPP = null;//用于Socket通信的IP地址和端口
+        private Socket socket = null;
+        private Socket clientSocket = null;
+        private NetworkStream nStream = null;//网络访问基础数据流
+        private TextReader tReader = null;//创建读取器
+        private TextWriter tWriter = null;//创建编写器
+
+        public void AcceptMessage()
+        {
+            clientSocket = socket.Accept();
+            if (clientSocket!=null)
+            {
+                bConnected = true;
+                this.Label1.Text = "与客户" + clientSocket.RemoteEndPoint.ToString() + "成功建立连接";
+            }
+            nStream = new NetworkStream(clientSocket);
+            tReader = new StreamReader(nStream);//读字节流
+            tWriter = new StreamWriter(nStream);//写字节流
+            string sTemp;//临时存储读取的字符串
+            while (bConnected)
+            {
+                try
+                {
+                    //连续从当前流中读取字符串直至结束
+                    sTemp = tReader.ReadLine();
+                    if (sTemp.Length!=0)
+                    {
+                        //RichTextBox2_KeyPress()和AcceptMessage()都将向
+                        //RichTextBox1写入字符,可能冲突,需要多线程互斥
+                        lock (this)
+                        {
+                            RichTextBox1.Text = "客户机:" + sTemp + "\n" + RichTextBox1.Text;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    tAcceptMsg.Abort();
+                    MessageBox.Show("无法与客户机通信!");
+                }
+            }
+            //禁止当前Socket上的发送与接收
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();//关闭Socket并释放所有关联资源
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+        private void Btn_Start_Click(object sender, EventArgs e)//启动侦听并显示聊天信息
+        {
+            //服务器侦听端口可以预先指定,这里使用最大端口值
+            //Any表示服务器应侦听所有网络接口上的客户活动
+            IPP = new IPEndPoint(IPAddress.Any, 65535);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(IPP);//绑定节点
+            socket.Listen(0);//0表示连接数量不限
+            tAcceptMsg = new Thread(new ThreadStart(this.AcceptMessage));
+            tAcceptMsg.Start();
+            Btn_Start.Enabled = false;
+        }
+
+        private void RichTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar==(char)13)//按下的是回车
+            {
+                if (bConnected)
+                {
+                    try
+                    {
+                        //RichTextBox2_KeyPress()和AcceptMessage()都将向
+                        //RichTextBox1写入字符,可能冲突,需要多线程互斥
+                        lock (this)
+                        {
+                            RichTextBox1.Text = "服务器:" + RichTextBox2.Text + RichTextBox2.Text + RichTextBox1.Text;
+                            //客户机聊天信息写入网络流便于服务器接收
+                            tWriter.WriteLine(RichTextBox2.Text);
+                            tWriter.Flush();//清理当前缓冲区,使缓冲数据写入基础设备
+                            RichTextBox2.Text = "";//发送成功后,清空输入框并聚焦
+                            RichTextBox2.Focus();
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("无法与客户机通信!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("未与客户机建立连接,无法与客户机通信!");
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                socket.Close();
+                tAcceptMsg.Abort();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
